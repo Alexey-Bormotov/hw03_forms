3e14d4ab4a8fc03cb96e8c064e9a101cef3e06e6
Alexey Bormotov <di-devil@yandex.ru>
2021-10-21 21:31:27 +0700

    Sprint:4, Commit:2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -15,8 +15,6 @@ class Group(models.Model):
 
 class Post(models.Model):
     text = models.TextField(
-        verbose_name='Текст поста',
-        help_text='Текст нового поста'
     )
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
@@ -30,8 +28,6 @@ class Post(models.Model):
         null=True,
         on_delete=models.SET_NULL,
         related_name='posts',
-        verbose_name='Группа',
-        help_text='Группа, к которой будет относиться пост'
     )
 
     class Meta:
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,12 +8,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+SHOW_POSTS = 10
+
 
 def index(request):
     template = 'posts/index.html'
     posts_all = Post.objects.all()
 
-    paginator = Paginator(posts_all, 10)
+    paginator = Paginator(posts_all, SHOW_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -28,7 +30,7 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts_all = group.posts.all()
 
-    paginator = Paginator(posts_all, 10)
+    paginator = Paginator(posts_all, SHOW_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -45,7 +47,7 @@ def profile(request, username):
     posts_all = author.posts.all()
     posts_count = author.posts.count()
 
-    paginator = Paginator(posts_all, 10)
+    paginator = Paginator(posts_all, SHOW_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -60,12 +62,10 @@ def profile(request, username):
 def post_detail(request, post_id):
     template = 'posts/post_detail.html'
     post = get_object_or_404(Post, pk=post_id)
-    post_slice = post.text[0:30]
     posts_count = post.author.posts.count()
 
     context = {
         'post': post,
-        'post_slice': post_slice,
         'posts_count': posts_count,
     }
     return render(request, template, context)
@@ -73,28 +73,26 @@ def post_detail(request, post_id):
 
 @login_required
 def post_create(request):
+    form = PostForm(request.POST or None)
     if request.method == 'POST':
-        post_temp = Post(author=request.user)
-        form = PostForm(request.POST, instance=post_temp)
 
         if form.is_valid():
-            form.save()
+            post_temp = form.save(commit=False)
+            post_temp.author = request.user
+            post_temp.save()
             return redirect('posts:profile', username=request.user.username)
 
         return render(request, 'posts/create_post.html', {'form': form})
-    else:
-        form = PostForm()
 
     return render(request, 'posts/create_post.html', {'form': form})
 
 
 @login_required
 def post_edit(request, post_id):
-    is_edit = True
-    post = Post.objects.get(pk=post_id)
+    post = get_object_or_404(Post, pk=post_id)
+    form = PostForm(request.POST or None, instance=post)
 
     if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
 
         if form.is_valid():
             form.save()
@@ -102,14 +100,10 @@ def post_edit(request, post_id):
 
         return render(request, 'posts/create_post.html', {
             'form': form,
-            'is_edit': is_edit,
             'post_id': post_id,
         })
 
-    form = PostForm(instance=post)
-
     return render(request, 'posts/create_post.html', {
         'form': form,
-        'is_edit': is_edit,
         'post_id': post_id,
     })
